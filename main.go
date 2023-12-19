f5d039279bc6212100fe4427846821867e0310d6
José Carlos Cuevas <reset.reboot@gmail.com>
2023-12-19 11:42:03 +0100

    fix: remove superflous code

--- main.go
+++ main.go
@@ -320,7 +320,6 @@ func main() {
 			host := ""
 			if len(broadcastResponse.Broadcast.NextShow.Show.Hosts) > 0 {
 				host = "by: " + broadcastResponse.Broadcast.NextShow.Show.Hosts[0].Name
-			} else {
 			}
 			showCard.SetSubTitle(date + " " + host)
 			showAvatar.Image = loadImageURL(broadcastResponse.Broadcast.NextShow.Show.AvatarUrl)
